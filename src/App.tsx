c44a6dd21e5c645c1bce25dc5d1ed2e2e0bb3858
Vaibhav Kulshrestha <vaibhavkulshrestha55@gmail.com>
2026-02-12 03:51:42 +0530

    removed footer

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,6 @@ import { ModernAchievements } from './components/sections/ModernAchievements';
 import { GithubSection } from './components/sections/github/GithubSection';
 import { ModernContact } from './components/sections/ModernContact';
 import { About } from './components/sections/About';
-import { Footer } from './components/layout/Footer';
 import { Spotlight } from './components/ui/spotlight';
 import CustomCursor from './components/CustomCursor';
 import Oneko from './components/oneko/oneko'; 
@@ -73,7 +72,7 @@ function App() {
           <Route path="/about" element={<About />} />
           <Route path="/experience" element={<Experience />} />
         </Routes>
-        <Footer />
+       
         <Analytics/>
       </div>
     </Router>
